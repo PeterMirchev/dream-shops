9aa2e6b6393aa5bca67871e0c9cb431b721636d6
Peter <guitaronline89@gmail.com>
2024-11-05 11:25:38 +0200

    CartService implementation.

---
+++ src/main/java/com/dailycodework/dreamshops/service/cart/CartService.java
@@ -0,0 +1,49 @@
+package com.dailycodework.dreamshops.service.cart;
+
+import com.dailycodework.dreamshops.exception.ResourceNotFoundException;
+import com.dailycodework.dreamshops.model.Cart;
+import com.dailycodework.dreamshops.model.CartItem;
+import com.dailycodework.dreamshops.repository.CartItemRepository;
+import com.dailycodework.dreamshops.repository.CartRepository;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class CartService implements ICartService {
+    private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
+
+    public CartService(CartRepository cartRepository, CartItemRepository cartItemRepository) {
+        this.cartRepository = cartRepository;
+        this.cartItemRepository = cartItemRepository;
+    }
+
+    @Override
+    public Cart getCart(Long id) {
+
+        Cart cart = cartRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Card Not Found! Invalid Card ID - %s", id)));
+
+        BigDecimal totalAmount = cart.getTotalAmount();
+        cart.setTotalAmount(totalAmount);
+
+        return cartRepository.save(cart);
+    }
+
+    @Override
+    public void clearCart(Long id) {
+
+        Cart cart = getCart(id);
+        cartItemRepository.deleteAllByCartId(id);
+        cart.getItems().clear();
+        cartRepository.deleteById(id);
+
+    }
+
+    @Override
+    public BigDecimal getTotalPrice(Long id) {
+
+        return getCart(id).getTotalAmount();
+    }
+}
