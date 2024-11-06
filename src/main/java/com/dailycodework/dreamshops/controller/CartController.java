e60e4e9bd10e9012d1dde61a02f3b6496c44b1eb
Peter <guitaronline89@gmail.com>
2024-11-06 17:03:41 +0200

    CartController implementation.

---
+++ src/main/java/com/dailycodework/dreamshops/controller/CartController.java
@@ -0,0 +1,43 @@
+package com.dailycodework.dreamshops.controller;
+
+import com.dailycodework.dreamshops.model.Cart;
+import com.dailycodework.dreamshops.response.ApiResponse;
+import com.dailycodework.dreamshops.service.cart.ICartService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("${api.prefix}/carts")
+public class CartController {
+    private final ICartService cartService;
+
+    public CartController(ICartService cartService) {
+        this.cartService = cartService;
+    }
+
+    @GetMapping("/{cartId}/my-cart")
+    private ResponseEntity<ApiResponse> getCart(@PathVariable Long cartId) {
+
+        Cart cart = cartService.getCart(cartId);
+
+        return ResponseEntity.ok(new ApiResponse("Success:", cart));
+    }
+
+    @DeleteMapping("/{cartId}/clear")
+    public ResponseEntity<ApiResponse> clearCart(@PathVariable Long cartId) {
+
+        cartService.clearCart(cartId);
+
+        return ResponseEntity.ok(new ApiResponse("Clear Cart Success!", null));
+    }
+
+    @GetMapping("/{cartId}/cart/total-price")
+    public ResponseEntity<ApiResponse> getTotalAmount(@PathVariable Long cartId) {
+
+        BigDecimal totalPrice = cartService.getTotalPrice(cartId);
+
+        return ResponseEntity.ok(new ApiResponse("Total Price:", totalPrice));
+    }
+}
