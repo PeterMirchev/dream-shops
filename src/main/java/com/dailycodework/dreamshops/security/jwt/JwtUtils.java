c4a6dfdac52f0d486a9ac1d7e118bc07e900806f
Peter <guitaronline89@gmail.com>
2024-11-22 23:27:10 +0200

    JWT implementation.

--- src/main/java/com/dailycodework/dreamshops/security/jwt/JwtUtils.java
+++ src/main/java/com/dailycodework/dreamshops/security/jwt/JwtUtils.java
@@ -1,17 +1,19 @@
 package com.dailycodework.dreamshops.security.jwt;
 
 import com.dailycodework.dreamshops.security.user.ShopUserDetails;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
 import java.util.List;
 
+@Component
 public class JwtUtils {
 
     private String jwtSecret;
@@ -45,6 +47,18 @@ public class JwtUtils {
                 .getSubject();
     }
 
+    public boolean validateToken(String token) {
+
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | SignatureException | IllegalArgumentException e) {
+            throw new JwtException(e.getMessage());
+        }
+    }
     private Key key() {
 
         return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
