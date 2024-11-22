62c64b84937da6e79028dd1c12be3520b1f16f04
Peter <guitaronline89@gmail.com>
2024-11-22 23:15:34 +0200

    JWT implementation.

---
+++ src/main/java/com/dailycodework/dreamshops/security/jwt/JwtUtils.java
@@ -0,0 +1,55 @@
+package com.dailycodework.dreamshops.security.jwt;
+
+import com.dailycodework.dreamshops.security.user.ShopUserDetails;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static jdk.internal.org.jline.keymap.KeyMap.key;
+
+public class JwtUtils {
+
+    private String jwtSecret;
+    private int expirationTime;
+
+    public String generateTokenForUser(Authentication authentication) {
+
+        ShopUserDetails userPrincipal = (ShopUserDetails) authentication.getPrincipal();
+
+        List<String> roles = userPrincipal.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+
+        return Jwts.builder()
+                .setSubject(userPrincipal.getEmail())
+                .claim("id", userPrincipal.getId())
+                .claim("roles", roles)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + expirationTime))
+                .signWith(key(), SignatureAlgorithm.HS256).compact();
+    }
+
+    public String getUsernameFromToken(String token) {
+
+        return Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    private Key key() {
+
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+}
--- src/main/java/com/dailycodework/dreamshops/security/user/ShopUserDetailsService.java
+++ src/main/java/com/dailycodework/dreamshops/security/user/ShopUserDetailsService.java
@@ -1,5 +1,8 @@
 package com.dailycodework.dreamshops.security.user;
 
+import com.dailycodework.dreamshops.exception.ResourceNotFoundException;
+import com.dailycodework.dreamshops.model.User;
+import com.dailycodework.dreamshops.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,8 +10,19 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ShopUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public ShopUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found! Invalid Email - %s".formatted(email)));
+
+        return ShopUserDetails.buildUserDetails(user);
     }
 }
